25f9a80322df43554647fc98c39a74cbb9fa767e
Jonathan Shin <jonathanwshin@gmail.com>
2015-08-19 22:39:00 -0400

    UTC

--- routes/index.js
+++ routes/index.js
@@ -150,7 +150,7 @@ router.get('/api/v1/arduino/:userId', function(req, res, next){
             adt = retValue[0].usage * (.98 / 30);
         }
 
-        console.log(date.toUTCString().yyyymmdd());
+        console.log(date.yyyymmdd());
 
         var dayUsageQuery = client.query('SELECT * FROM waterusage_by_day WHERE userId=($1) and date=($2)', [req.params.userId, date.yyyymmdd()]);
 
