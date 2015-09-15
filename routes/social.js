c7945b0bd0bf6abe37328fb1d348af5034f85d8b
Jonathan <jonathanwshin@gmail.com>
2015-09-14 22:26:46 -0400

    Adding comments for reminder

--- routes/social.js
+++ routes/social.js
@@ -15,6 +15,7 @@ router.get('/login', function (req, res, next) {
 });
 
 router.get('/login/auth/facebook', function(req, res, next) {
+    /* Need authorization logic for facebook */
     res.sendfile('public/social/main.html');
 });
 
