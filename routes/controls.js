7090977334513ec0ff6303e421e8152e5c5018b1
jonathan <jonathanwshin@gmail.com>
2016-03-19 00:09:57 -0400

    Adding results on different endpoints

--- routes/controls.js
+++ routes/controls.js
@@ -14,6 +14,18 @@ router.get('/state', function(req, res) {
   res.send('"'+ seedType + '|' + sectionDistance + '|' + totalDistance + '"');
 });
 
+router.get('/state/seedType', function(req, res) {
+  res.send('"' + seedType + '"');
+});
+
+router.get('/state/sectionDistance', function(req, res) {
+  res.send('"' + sectionDistance + '"');
+});
+
+router.get('/state/totalDistance', function(req, res) {
+  res.send('"' + totalDistance+ '"');
+});
+
 router.post('/values', function(req, res) {
   seedType = req.body.seedType;
   sectionDistance = req.body.sectionDistance;
